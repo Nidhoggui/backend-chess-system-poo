899df56d0bb8fda571a0fb73d35bcc89054a978e
Nidhoggui <guilhermemelquiades42@gmail.com>
2022-09-14 15:24:57 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -70,4 +70,21 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
+	public Piece removePiece(Position position)
+	{
+		if(!positionExists(position))
+		{
+			throw new BoardException("Sem Resenha!");
+		}
+		
+		if(piece(position) == null)
+		{
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 }
