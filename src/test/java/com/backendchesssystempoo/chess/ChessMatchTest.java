156d806c3115921379025bbfd2adf4c754292c49
Kire87 <erikff7@gmail.com>
2022-10-03 13:24:22 -0300

    tests

--- src/test/java/com/backendchesssystempoo/chess/ChessMatchTest.java
+++ src/test/java/com/backendchesssystempoo/chess/ChessMatchTest.java
@@ -1,15 +1,34 @@
 package com.backendchesssystempoo.chess;
 
 import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
+
+import com.backendchesssystempoo.chess.pieces.King;
 
 class ChessMatchTest {
 
 	@Test
-	void test() {
+	void testCheckmate() {
 		ChessMatch chessMatch = new ChessMatch();
+		assertFalse(chessMatch.getCheckMate());
+	}
+	
+	@Test
+	void testKingPosition() {
+		ChessMatch chessMatch = new ChessMatch();
+		ChessPiece[][] pieces = chessMatch.getPieces();
 		
+		assertTrue(pieces[0][4] instanceof King);
+	}
+	
+	@Test
+	void testVacantPosition() {
+		ChessMatch chessMatch = new ChessMatch();
+		ChessPiece[][] pieces = chessMatch.getPieces();
+		
+		assertTrue(pieces[2][2] == null);
 	}
 
 }
